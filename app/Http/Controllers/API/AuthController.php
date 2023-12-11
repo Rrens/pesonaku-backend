1738b9d77fc39f9b6edf3af1bc0578b7db352928
Rrens <yusufrendi313@gmail.com>
2023-12-11 10:16:30 +0700

    UUID

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Exception;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => ['login', 'register']]);
+    }
+
+    public function login()
+    {
+        $credentials = request(['email', 'password']);
+
+        if (!$token = auth()->attempt($credentials)) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        return $this->respondWithToken($token);
+    }
+
+    public function register(Request $request)
+    {
+        try {
+
+            $validator = Validator::make($request->all(), [
+                'name' => 'required',
+                'email' => 'required|email|unique:users',
+                'password' => 'required'
+            ]);
+
+            if ($validator->fails()) {
+                return response()->json([
+                    'meta' => [
+                        'status' => 'Failed',
+                        'message' => 'Bad Requst'
+                    ],
+                    'data' => $validator->messages()->all()
+                ], 400);
+            }
+
+            $data = new User();
+            $data->id = \Ramsey\Uuid\Uuid::uuid4()->toString();
+            $data->name = $request->name;
+            $data->email = $request->email;
+            $data->password = Hash::make($request->password);
+            $data->role = 2;
+            $data->save();
+
+            $data_token = User::findOrFail($data->id);
+            $tokenResult = $data_token->createToken('authToken')->plainTextToken;
+            $data_token->remember_token = $tokenResult;
+            $data_token->save();
+
+            return response()->json([
+                'meta' => [
+                    'status' => 'success',
+                    'message' => 'Please Save Your Token for access the data'
+                ],
+                'data' => [
+                    'access_token' => $tokenResult,
+                    'token_type' => 'Bearer',
+                ]
+            ], 201);
+        } catch (Exception $error) {
+            return response()->json([
+                'meta' => [
+                    'status' => 'error',
+                    'message' => 'Something went wrong'
+                ],
+                'data' => $error->getMessage()
+            ], 500);
+        }
+    }
+
+    public function me()
+    {
+        return response()->json(auth()->user());
+    }
+
+    public function logout()
+    {
+        auth()->logout();
+
+        return response()->json(['message' => 'Successfully logged out']);
+    }
+
+    public function refresh()
+    {
+        return $this->respondWithToken(auth()->refresh());
+    }
+
+    protected function respondWithToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+        ]);
+    }
+}
